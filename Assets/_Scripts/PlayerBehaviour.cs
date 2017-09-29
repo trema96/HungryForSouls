2743e66f5ea30b7c5c47adb1d8ee4ff37effe15d
Tremamunno Luca <luca.tremamunno@studio.unibo.it>
2017-09-29 21:08:17 +0200

    Initial

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hMov = Input.GetAxis("Horizontal");
+        float vMov = Input.GetAxis("Vertical");
+
+        transform.Translate(hMov * speed * Time.deltaTime, 0, vMov * speed * Time.deltaTime);
+    }
+}
