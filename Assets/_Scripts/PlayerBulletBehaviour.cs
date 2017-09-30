f564a40505864cb1163f014a6d9076f819a6559b
Tremamunno Luca <luca.tremamunno@studio.unibo.it>
2017-09-30 11:51:03 +0200

    Commit luca

---
+++ Assets/_Scripts/PlayerBulletBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletBehaviour : MonoBehaviour {
+    public float speed;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+	}
+}
