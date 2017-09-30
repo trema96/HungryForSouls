e8240d467a4a5f232fe73f0b6d43e03ecd8feb03
Alexander Oliver Nymann Hvidt <alexander2341@gmail.com>
2017-09-30 12:27:56 +0200

    TmpCommit

---
+++ Assets/GhostMovementBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovementBehaviour : MonoBehaviour {
+    Transform player;
+    float movementSpeed = 100f;
+
+    // Use this for initialization
+    void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //gameObject.transform      
+    }
+}
